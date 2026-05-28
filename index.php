0a4089d45e537853de835dcfbd1fa513e7db9d9f
Doukoure <doukmahmoud@gmail.com>
2026-05-28 18:43:30 +0000

    Add index.php

--- index.php
+++ index.php
@@ -4,8 +4,3 @@ header('Content-Type: application/json');
 
 require 'db.php';
 
-echo json_encode([
-    "success" => true,
-    "message" => "Render API + MySQL works",
-    "server_time" => date("Y-m-d H:i:s")
-]);
